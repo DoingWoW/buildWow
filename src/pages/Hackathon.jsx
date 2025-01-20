4d93980ae73fe61a179ce75574cab444bb9de6a9
Naman Chandok <namanchandok1@gmail.com>
2025-01-20 22:45:31 +0530

    pagination and tailwind installation

---
+++ src/pages/Hackathon.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Hackathon() {
+  return (
+    <main className='min-h-screen bg-black text-white'>
+        <h1>Hackathon</h1>
+        <a href="/">Go to landing page</a>
+    </main>
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <main className='min-h-screen bg-black text-white'>
+        <h1>Home</h1>
+        <a href="/hack">Go to hackathon page</a>
+    </main>
+  )
+}
